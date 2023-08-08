794fcc4b380ab11c0e423c8940aaf9a21706830d
Ramón Huidobro <hola@ramonh.dev>
2023-08-08 13:04:26 +0200

    Adapt to recording status

--- src/utils/githubRepos.ts
+++ src/utils/githubRepos.ts
@@ -1,27 +1,17 @@
-import Repo from '../types/Repo.ts';
+import type Repo from '../types/Repo';
 
-export async function queryReposForUser(username: String): Repo[] {
-  const repoParams = {
+export async function queryReposForUser(username: string): Promise<Repo[]> {
+  const repoParams: Record<'sort' | 'direction' | 'per_page', string> = {
     sort: 'created',
     direction: 'descending',
-    per_page: 10,
+    per_page: '10',
   };
 
+  const searchParams = new URLSearchParams(repoParams).toString();
+  console.log(`https://api.github.com/users/${username}/repos?${searchParams}`);
   const response = await fetch(
-    `https://api.github.com/users/${username}/repos?${new URLSearchParams(
-      repoParams,
-    ).toString()}`,
+    `https://api.github.com/users/${username}/repos?${searchParams}`,
   );
-  const repos: [{ name: string; description: string; html_url: string }] =
-    await response.json();
-  return repos;
 
-  return await Promise.all(
-    repos.map(
-      async (repo: { name: string; description: string; html_url: string }) => {
-        const { result } = await ogs({ url: repo.html_url });
-        return { ...repo, ogImage: result.ogImage[0] };
-      },
-    ),
-  );
+  return response.json();
 }
